e9e7a887a549769b9312acc32007953ffd6be11a
Frederik Pasch <fred-labs@mailbox.org>
2026-02-10 21:24:51 +0100

    Fix empty pre/post command

--- src/robovast/execution/data/entrypoint.sh
+++ src/robovast/execution/data/entrypoint.sh
@@ -79,22 +79,26 @@ if [ "$#" -ne 0 ]; then
     exec "$@"
 else
     # Validate PRE_COMMAND exists if specified
-    if [ -e "${PRE_COMMAND}" ]; then
-        log "Executing pre-command: ${PRE_COMMAND}"
-        source "${PRE_COMMAND}"
-    else
-        log "ERROR: Pre-command '${PRE_COMMAND}' does not exist."
-        exit 1
+    if [ -n "${PRE_COMMAND}" ]; then
+        if [ -e "${PRE_COMMAND}" ]; then
+            log "Executing pre-command: ${PRE_COMMAND}"
+            source "${PRE_COMMAND}"
+        else
+            log "ERROR: Pre-command '${PRE_COMMAND}' does not exist."
+            exit 1
+        fi
     fi
     
     # Validate POST_COMMAND exists if specified
     POST_COMMAND_PARAM=""
-    if [ -e "${POST_COMMAND}" ]; then
-        POST_COMMAND_PARAM="--post-run ${POST_COMMAND}"
-        log "Post-command '${POST_COMMAND}' will be executed after scenario execution."
-    else
-        log "ERROR: Post-command '${POST_COMMAND}' does not exist."
-        exit 1
+    if [ -n "${POST_COMMAND}" ]; then
+        if [ -e "${POST_COMMAND}" ]; then
+            POST_COMMAND_PARAM="--post-run ${POST_COMMAND}"
+            log "Post-command '${POST_COMMAND}' will be executed after scenario execution."
+        else
+            log "ERROR: Post-command '${POST_COMMAND}' does not exist."
+            exit 1
+        fi
     fi
     
     if [ -e /config/scenario.config ]; then
