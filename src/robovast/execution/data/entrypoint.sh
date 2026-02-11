455af67f738e477a9e5048568925b361e51dbf0f
Frederik Pasch <fred-labs@mailbox.org>
2026-02-11 07:41:04 +0100

    config files in final dataset only once

--- src/robovast/execution/data/entrypoint.sh
+++ src/robovast/execution/data/entrypoint.sh
@@ -69,11 +69,6 @@ echo "RUN_NUM: $RUN_NUM" >> ${OUTPUT_DIR}/run.yaml
 echo "SCENARIO_ID: $SCENARIO_ID" >> ${OUTPUT_DIR}/run.yaml
 echo "SCENARIO_CONFIG: $SCENARIO_CONFIG" >> ${OUTPUT_DIR}/run.yaml
 
-if [ -d /config ]; then
-  log "Copying configuration files..."
-  cp -r /config/* ${OUTPUT_DIR}/
-fi
-
 if [ "$#" -ne 0 ]; then
     log "Executing custom command: $@"
     exec "$@"
