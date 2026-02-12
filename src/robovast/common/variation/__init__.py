23f5aa9ec55f36ee592aa8a358ee804283bc92d5
Frederik Pasch <fred-labs@mailbox.org>
2026-02-12 22:54:32 +0100

    fixes

--- src/robovast/common/variation/__init__.py
+++ src/robovast/common/variation/__init__.py
@@ -6,17 +6,17 @@ from .parameter_variation import (ParameterVariationDistributionGaussian,
 # Lazy import for GUI classes to avoid loading PySide6 in headless environments
 def __getattr__(name):
     if name == 'VariationGui':
-        from .base_variation_gui import VariationGui
+        from .base_variation_gui import VariationGui  # pylint: disable=import-outside-toplevel
         return VariationGui
     elif name == 'VariationGuiRenderer':
-        from .base_variation_gui import VariationGuiRenderer
+        from .base_variation_gui import VariationGuiRenderer  # pylint: disable=import-outside-toplevel
         return VariationGuiRenderer
     raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
 
 __all__ = [
     'Variation',
-    'VariationGui',
-    'VariationGuiRenderer',
+    'VariationGui',  # pylint: disable=undefined-all-variable
+    'VariationGuiRenderer',  # pylint: disable=undefined-all-variable
     'ParameterVariationList',
     'ParameterVariationDistributionUniform',
     'ParameterVariationDistributionGaussian',
